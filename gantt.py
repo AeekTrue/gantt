73d1f5733dfe154d6073b51244b29ff2eff0403d
Aeek True <aeektrue@gmail.com>
2025-03-26 11:33:21 +0300

    Revert "finalize colorization"
    This reverts commit 70976155e42ebe3289ebebe92bab98e1c5a5f628.

--- gantt.py
+++ gantt.py
@@ -125,9 +125,9 @@ def display_timeline(lshift=0, start_date: dt.datetime=None, end_date: dt.dateti
 
         style = colorama.Fore.BLACK
         if weekend:
-            style += colorama.Back.LIGHTRED_EX if (date.weekday()) % 2 else colorama.Back.RED
+            style += colorama.Back.LIGHTRED_EX if (i+1) % 2 else colorama.Back.RED
         else:
-            style += colorama.Back.WHITE if (date.weekday()) % 2 else colorama.Back.LIGHTBLACK_EX
+            style += colorama.Back.WHITE if (i+1) % 2 else colorama.Back.LIGHTBLACK_EX
         row0 += make_up(weekday, style, offset=0)
 
 
